0c40307f9ea820cc13441b4c9b5a832b66441ec5
Josef Kuchař <email@josefkuchar.cz>
2017-10-07 17:51:42 +0200

    Format configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const path = require('path');
 
 module.exports = {
     entry: {
-      'watan': './src/client/index.ts',
-      'watan.min': './src/client/index.ts'
+        'watan': './src/client/index.ts',
+        'watan.min': './src/client/index.ts'
     },
     output: {
-      path: path.resolve('_bundles'),
-      filename: '[name].js',
-      libraryTarget: 'umd',
-      library: 'watan',
-      umdNamedDefine: true
+        path: path.resolve('_bundles'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'watan',
+        umdNamedDefine: true
     },
     resolve: {
-      extensions: ['.ts', '.tsx']
+        extensions: ['.ts', '.tsx']
     },
     devtool: 'source-map',
     plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        sourceMap: true,
-        include: /\.min\.js$/,
-      })
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: true,
+            include: /\.min\.js$/,
+        })
     ],
     module: {
-      loaders: [{
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader?configFileName=tsconfig.client.json'
-      }]
+        loaders: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader?configFileName=tsconfig.client.json'
+        }]
     }
-  }
+}
