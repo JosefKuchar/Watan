35c50fadf42496f12e717d7d8415f9f680797337
Josef Kuchař <email@josefkuchar.cz>
2017-10-07 17:02:37 +0200

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+      'watan': './src/client/index.ts',
+      'watan.min': './src/client/index.ts'
+    },
+    output: {
+      path: path.resolve('_bundles'),
+      filename: '[name].js',
+      libraryTarget: 'umd',
+      library: 'watan',
+      umdNamedDefine: true
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx']
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        sourceMap: true,
+        include: /\.min\.js$/,
+      })
+    ],
+    module: {
+      loaders: [{
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader?configFileName=tsconfig.client.json'
+      }]
+    }
+  }
